855a54a8c1f98d5ba3bcea60914838261850af79
Sayanth Rock <202829406+SayanthRock@users.noreply.github.com>
2026-06-23 08:13:06 +0530

    fix: include customer options in module zip

--- scripts/package.sh
+++ scripts/package.sh
@@ -17,8 +17,9 @@ FILES=(
   "system_ext"
   "themes"
   "docs"
+  "customer-options"
 )
 
 printf 'Building %s\n' "$ZIP_NAME"
-zip -r "$OUT_DIR/$ZIP_NAME" "${FILES[@]}" -x "*.git*" -x "dist/*"
+zip -r "$OUT_DIR/$ZIP_NAME" "${FILES[@]}" -x "*.git*" -x "dist/*" -x "*.DS_Store"
 printf 'Done: %s/%s\n' "$OUT_DIR" "$ZIP_NAME"
